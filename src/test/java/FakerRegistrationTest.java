b2a3ada614533002a8fb9262c4e866ac510eeeeb
NadezhdaBul <Bulygina_n_a@mail.ru>
2023-04-02 21:55:59 +0300

    Jenkins_13lesson

--- src/test/java/FakerRegistrationTest.java
+++ src/test/java/FakerRegistrationTest.java
@@ -8,7 +8,7 @@ import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
 
-public class FakerRegistrationTest {
+public class FakerRegistrationTest extends TestBase{
         RegistrationPage registrationPage = new RegistrationPage();
 
 
